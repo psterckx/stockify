ee68c15ba5b06f7d8141745dc9018e86da823c00
Peter Sterckx <pasterckx@gmail.com>
2019-11-02 02:19:35 -0500

    enter stock and quant using wtf forms

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some_string'
+
+from stockify.forms import StockForm
 
 # Homepage
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
 
 # About
 @app.route('/about', methods=['GET','POST'])
@@ -13,5 +17,13 @@ def about():
 		return render_template('about.html', name=request.form['name'], age=request.form['age'])
 	else:
 		return render_template('about.html')
+
+@app.route('/stockpage',methods=['GET','POST'])
+def stocks():
+    form = StockForm()
+    if form.validate_on_submit():
+        flash('Bought {} stocks of {}.'.format(form.quantity.data,form.ticker.data))
+        return redirect('/stockpage')
+    return render_template('stockpage.html',form=form)
 
 app.run()

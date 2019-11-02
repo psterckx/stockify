d6e1ded77d31df949e6a2cc3e3bfb0a1fd84f627
Peter Sterckx <pasterckx@gmail.com>
2019-11-02 15:34:49 -0500

    connecting to Spotify API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, redirect, flash
+import spotipy
+import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some_string'
 
@@ -25,5 +27,37 @@ def stocks():
         flash('Bought {} stocks of {}.'.format(form.quantity.data,form.ticker.data))
         return redirect('/stockpage')
     return render_template('stockpage.html',form=form)
+
+@app.route('/music',methods=['GET','POST'])
+def show_playlist():
+
+    try:
+        sp = spotipy.Spotify(auth=access_token)
+    except:
+        client_id = '76dc13fd61724db2b90c395de1ada189'
+        client_secret = '84d95362cc1742b19951408699b269b2'
+
+        grant_type = 'client_credentials'
+        body_params = {'grant_type' : grant_type}
+
+        url='https://accounts.spotify.com/api/token'
+
+        r=requests.post(url, data=body_params, auth = (client_id, client_secret))
+        data = r.json()
+        access_token = data['access_token']
+
+        sp = spotipy.Spotify(auth=access_token)
+
+    happytracks = sp.search(q='happy', limit=20, type='playlist')
+
+    list = happytracks['playlists']['items']
+
+    first = list[0]['name']
+
+    flash('Play some {}.'.format(first))
+    # return redirect('/music')
+    return render_template('music.html')
+
+
 
 app.run()
